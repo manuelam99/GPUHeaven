f959b25bef0f581dadc87db38abd5b79ceb21db2
Manuel Alvarez <manuelam2999@gmail.com>
2021-05-03 12:09:40 -0500

    formato general de pagina de usuario

--- php/usuario.php
+++ php/usuario.php
@@ -19,6 +19,27 @@ session_start();
     <title>GPU Heaven</title>
 </head>
 
+<?php
+$con = mysqli_connect("localhost", "root", "root", "gpu_heaven");
+// Check connection
+if (mysqli_connect_errno()) {
+    $cards = "error";
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$query = "SELECT username, nom_usuario, email_usuario, fecha_nac, tarj_usuario, direc_usuario 
+                                    FROM usuarios WHERE id_usuario = '1';";
+
+$result = mysqli_query($con, $query);
+
+$usuario = mysqli_fetch_row($result);
+
+mysqli_close($con);
+
+
+
+?>
+
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <!-- Brand -->
@@ -44,7 +65,79 @@ session_start();
             <a href="#"><span class="oi oi-cart text-light" title="Cart" aria-hidden="true"></span></a>
         </div>
     </nav>
-    
+
+    <div class="container mt-3">
+        <h2>Nombre: <?php echo $usuario[1] ?></h2>
+        <h3>Username: <?php echo $usuario[0] ?></h3>
+        <hr />
+        <ul class="nav nav-tabs justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#info">Información</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#carrito">Carrito</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#compras">Historial de Compras</a>
+            </li>
+        </ul>
+
+        <div class="tab-content">
+            <div class="tab-pane container active mt-4" id="info">
+                <form action="">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Correo</span>
+                        </div>
+                        <input type="email" name="correo" id="correo" class="form-control" value="<?php echo $usuario[2] ?>" disabled>
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-warning">Modificar</button>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Fecha</span>
+                        </div>
+                        <input type="date" name="fecha" id="fecha" class="form-control" value="<?php echo $usuario[3] ?>" disabled>
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-warning">Modificar</button>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Tarjeta</span>
+                        </div>
+                        <input type="text" name="tarj" id="tarj" class="form-control" value="<?php echo $usuario[4] ?>" disabled>
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-warning">Modificar</button>
+                        </div>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Dirección</span>
+                        </div>
+                        <input type="text" name="direc" id="direc" class="form-control" value="<?php echo $usuario[5] ?>" disabled>
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-warning">Modificar</button>
+                        </div>
+                    </div>
+                    <button class="btn btn-success" type="submit" disabled>Guardar Cambios</button>
+                </form>
+            </div>
+            <div class="tab-pane container fade mt-4" id="carrito">
+
+            </div>
+            <div class="tab-pane container fade mt-4" id="compras">
+
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
